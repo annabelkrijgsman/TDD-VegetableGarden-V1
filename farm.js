49df9c9dfd9024b4eb9a35df1bb3b00cf471ba91
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2021-11-22 10:45:40 +0100

    getYieldForCrop + getTotalYield

--- farm.js
+++ farm.js
@@ -1,9 +1,29 @@
 const getYieldForPlant = (corn) => {
     if (corn.yield === 30) {
-        return 30;
+        return corn.yield;
     }
+};
+
+const getYieldForCrop = (input) => {
+    const total = input.crop.yield * input.numCrops;
+    return total;
+};
+
+const getTotalYield = ({ crops }) => {
+
+    const cornYield = crops[0].crop.yield;
+    const cornNum = crops[0].numCrops;
+    const totalCorn = cornYield * cornNum;
+
+    const pumpkinYield = crops[1].crop.yield;
+    const pumpkimNum = crops[1].numCrops;
+    const totalPumpkin = pumpkinYield * pumpkimNum;
+
+    return totalCorn + totalPumpkin;
 };
 
 module.exports = {
     getYieldForPlant,
+    getYieldForCrop,
+    getTotalYield
 };
